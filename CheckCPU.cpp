e653dca2c775b33df978defe7cad3f6794d5ce0d
xingzhexiaozhu <15671628302@163.com>
2016-03-18 09:44:21 +0800

    modify CheckCPU.cpp

--- CheckCPU.cpp
+++ CheckCPU.cpp
@@ -10,19 +10,34 @@
 //解释：
 //由于联合体union的存放顺序是所有成员都从低地址开始存放，利用该特
 //性就可以国松地获得CPU对内存采用LittleEndian还是BigEndian模式读写
+
+//方法一：
 int checkFunction() {
 	union {
 		int a;
 		char b;
 	}c;
 	c.a = 1;
-	return (c.b == 1);
+	return (c.b == 1);//大端模式则返回0，否则返回false
+}
+
+//方法二：
+bool checkFunction2() {
+	short int test = 0x1234;
+	if (*((char *)&test) == 0x12)
+		return true;//如果大端模式则返回true
+	else return false;//否则返回false
 }
 
 int main()
 {
 	int flag = checkFunction();
 	if(flag == 0)
+		printf("CPU is BigEndian!\n");
+	else printf("CPU is LittleEndian!\n");
+
+	int flag2 = checkFunction2();
+	if (flag2 == true)
 		printf("CPU is BigEndian!\n");
 	else printf("CPU is LittleEndian!\n");
 
