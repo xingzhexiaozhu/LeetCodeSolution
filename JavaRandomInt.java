8bb4d0cc938c338703ec95edc3c2622443f4703d
xingzhexiaozhu <15671628302@163.com>
2016-04-08 08:23:01 +0800

    JavaRandomInt.java

---
+++ JavaRandomInt.java
@@ -0,0 +1,30 @@
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class JavaRandomInt {
+
+	public static void main(String[] args) {
+		//Math.random()方法
+		for(int i=0; i<10; i++)
+			System.out.print((int)(Math.random()*100) + " ");
+		System.out.println();
+		
+		//不带种子，每次随机数不同
+		Random rand = new Random();
+		for(int i=0; i<10; i++)
+			System.out.print(rand.nextInt(100) + " ");
+		System.out.println();
+		
+		//带种子，每次随机数相同
+		Random rand2 = new Random(10);
+		for(int i=0; i<10; i++)
+			System.out.print(rand2.nextInt(100) + " ");
+		System.out.println();
+		
+		//生成完全不重复的随机数
+		SecureRandom random = new SecureRandom();
+		for(int i=0; i<10; i++)
+			System.out.print(random.nextInt(100) + " ");
+	}
+
+}
