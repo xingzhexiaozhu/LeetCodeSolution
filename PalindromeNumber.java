33441820d0c9e8474194636dfb98b3cd27f6ea19
xingzhexiaozhu <15671628302@163.com>
2016-05-10 21:29:08 +0800

    PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -10,11 +10,24 @@ public class PalindromeNumber {
 		return str.equals(new StringBuilder(str).reverse().toString());
 	}
 	
+	//解题思路二：处理数字，首先负数不是回文数字，其次反转各个位数，如果变换后数字与原数字相等则说明是回文
+	private static boolean isPalindrome1(int x){
+		if(x < 0)
+			return false;
+		int num = 0;
+		int temp= x;
+		while(temp != 0){
+			num = num*10 + (temp%10);
+			temp = temp/10;
+		}
+		return num==x;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNext()){
 			int x = sc.nextInt();
-			System.out.println(isPalindrome(x));
+			System.out.println(isPalindrome(x) + " " + isPalindrome1(x));
 		}
 	}
 }
