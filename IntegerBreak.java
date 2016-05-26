75ec5c0b4e372b1d1874585fecd68394876e0ee7
xingzhexiaozhu <15671628302@163.com>
2016-05-26 09:56:40 +0800

    modify IntegerBreak.java

--- IntegerBreak.java
+++ IntegerBreak.java
@@ -10,30 +10,44 @@ public class IntegerBreak {
 	//（1）可以发现因为4可以分解为2*2，而2*2的积又是4，所以可以数字分解到4就不必再细分；
 	//（2）分解因子全为2和3时乘积最大，同时要尽可能多的3；
 	//（3）当n%3==0时，因子全为3且3的个数为(n/3)乘积最大；当n%3==1时，因子3的个数为(n/3)-1乘积最大；当n%3==2时，因子3的个数为(n/3)
+//	private static int integerBreak(int n) {
+//		if(n < 2) return 0;
+//		else if(n == 2) 
+//			return 1;
+//		else if(n == 3)
+//			return 2;
+//		
+//		int mod = n % 3;//先得到模3的余数
+//		int product = 1;//最终的乘积
+//		int count = 0;  //因子3的个数
+//		if(mod == 1)
+//			count = n/3 - 1;
+//		else count = n/3;
+//		
+//		while(count > 0){
+//			product *= 3;
+//			--count;
+//		}
+//		
+//		if(mod == 1) product *= 4;
+//		else if(mod == 2) product *= 2;
+//			
+//		return product;
+//	}
 	private static int integerBreak(int n) {
-		if(n < 2) return 0;
-		else if(n == 2) 
-			return 1;
-		else if(n == 3)
-			return 2;
+		if(n < 0)
+			return 0;
+		if(n < 4)
+			return n-1;
 		
-		int mod = n % 3;//先得到模3的余数
-		int product = 1;//最终的乘积
-		int count = 0;  //因子3的个数
-		if(mod == 1)
-			count = n/3 - 1;
-		else count = n/3;
-		
-		while(count > 0){
+		int product = 1;
+		while(n > 4){
 			product *= 3;
-			--count;
+			n -= 3;
 		}
-		
-		if(mod == 1) product *= 4;
-		else if(mod == 2) product *= 2;
-			
-		return product;
+		return product * n;
 	}
+	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
